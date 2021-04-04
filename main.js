5a9492889efbf8761349fa329260c8c8c1e7eb84
Hao <haaochi@users.noreply.github.com>
2021-04-04 14:46:44 +0200

    Add autoslider

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const ul = document.querySelector('.j-img-list');
 const img_lists = document.querySelectorAll('.j-img-list > li');
 const img_list_length = img_lists.length;
 const first_img = document.querySelector('.j-img-list > li:first-child img');
+const left_auto = document.querySelector('.j-left-auto');
+const off_auto = document.querySelector('.j-off-auto');
+const right_auto = document.querySelector('.j-right-auto');
+const all_auto = document.querySelectorAll('.sliders-off');
+console.log(all_auto);
+
 
 ul.style.width = img_list_length + '00' + '%';
 
@@ -34,14 +40,52 @@ const prev = () => {
     });
 };
 
-
-
 for (const next_img of next_btn) {
     next_img.addEventListener('click', next);
-}
+};
 for (const prev_img of prev_btn) {
     prev_img.addEventListener('click', prev);
+};
+
+const remove_slider_class = () => {
+    for (const slider_on of all_auto) {
+        if (slider_on.classList.contains('slider-on')) {
+            slider_on.classList.remove('slider-on');
+        };
+    };
 }
+
+let interval = null;
+left_auto.addEventListener('click', () => {
+    clearInterval(interval);
+    remove_slider_class();
+    left_auto.textContent = 'Left Auto On';
+    right_auto.textContent = 'Right Auto Off';
+    off_auto.textContent = 'No sliding';
+    left_auto.classList.add('slider-on');
+    interval = setInterval(prev, 3000);
+});
+
+off_auto.addEventListener('click', () => {
+    clearInterval(interval)
+    remove_slider_class();
+    left_auto.textContent = 'Left Auto Off';
+    right_auto.textContent = 'Right Auto Off';
+    off_auto.textContent = 'No sliding';
+    off_auto.classList.add('slider-on');
+});
+
+right_auto.addEventListener('click', () => {
+    clearInterval(interval);
+    remove_slider_class();
+    left_auto.textContent = 'Left Auto Off';
+    right_auto.textContent = 'Right Auto On';
+    off_auto.textContent = 'No sliding';
+    right_auto.classList.add('slider-on');
+    interval = setInterval(next, 3000)
+});
+
+right_auto.click();
 
 
 
