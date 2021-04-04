fee822fab533f8bfb9f8d044f260093a5cf2ae91
Hao <haaochi@users.noreply.github.com>
2021-04-04 11:50:33 +0200

    Temporary fix for close button

--- navbar.js
+++ navbar.js
@@ -5,23 +5,30 @@ const nav = document.querySelector('.j-nav');
 const close_icon = document.querySelector('.close-hide');
 const burger_icon = document.querySelector('.j-burger-icon');
 const burger_nav = document.querySelector('.j-burger-nav');
-console.log(nav);
 
+console.log(burger_menu)
 //Burger menu close
 const close = () => {
     overlay.style.display = "none";
     burger_nav.style.display = "none";
     close_icon.style.display = 'none';
     burger_icon.style.display = 'block';
+    //Temporary fix for not closing bug
+    burger_menu.style.pointerEvents = 'all';
 };
 
 overlay.addEventListener('click', close);
+close_icon.addEventListener('click', close);
 
+
+burger_menu.classList.contains
 burger_menu.addEventListener('click', () => {
     overlay.style.display = "block";
     close_icon.style.display = 'block';
     burger_nav.style.display = "block";
     burger_icon.style.display = 'none';
+    //Temporary fix for not closing bug
+    burger_menu.style.pointerEvents = 'none';
 });
 
 
